b2e86a9714df196cceb9763e4d889142f2994277
Ishanvi Kotha <92087449+Ishanvi26@users.noreply.github.com>
2023-11-21 23:46:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 current_dir = os.getcwd()
 
 model_path = os.path.join(current_dir, 'payments.pkl')
+model = None  # Initialize model outside of conditional block
 
 if os.path.exists(model_path):
     try:
@@ -29,22 +30,30 @@ def home():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-        features = [
-            float(request.form['step']),
-            float(request.form['type']),
-            float(request.form['amount']),
-            float(request.form['oldbalanceOrg']),
-            float(request.form['newbalanceOrig']),
-            float(request.form['oldbalanceDest']),
-            float(request.form['newbalanceDest'])
-        ]
+        if model is not None:  # Check if the model is successfully loaded
+            try:
+                features = [
+                    float(request.form['step']),
+                    float(request.form['type']),
+                    float(request.form['amount']),
+                    float(request.form['oldbalanceOrg']),
+                    float(request.form['newbalanceOrig']),
+                    float(request.form['oldbalanceDest']),
+                    float(request.form['newbalanceDest'])
+                ]
 
-        df = pd.DataFrame([features], columns=['step', 'type', 'amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest'])
+                df = pd.DataFrame([features], columns=['step', 'type', 'amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest'])
 
-        prediction = model.predict(df)[0]
+                prediction = model.predict(df)[0]
 
-        result = "Fraud" if prediction == 1 else "Not Fraud"
+                result = "Fraud" if prediction == 1 else "Not Fraud"
 
-        return render_template('submit.html', result=result)
+                return render_template('submit.html', result=result)
+            except Exception as e:
+                print(f"Error during prediction: {e}")
+                traceback.print_exc()  # Print the stack trace for debugging
+                return render_template('error.html')  # Create an error page for users
+        else:
+            return render_template('error.html')  # Create an error page for users if the model is not loaded
 
     return render_template('predict.html')
